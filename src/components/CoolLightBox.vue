e29e998efc1bfbedff7bb9a4ce218d3a378da6fb
Miles Every <milesevery1234556@aol.com>
2021-12-10 12:57:24 -0500

    added download button

--- src/components/CoolLightBox.vue
+++ src/components/CoolLightBox.vue
@@ -316,6 +316,12 @@
               </path>
             </svg>
           </button>
+
+          <button type="button" @click="Download()" title="Download" class="cool-lightbox-toolbar__btn">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M17.064,4.656l-2.05-2.035C14.936,2.544,14.831,2.5,14.721,2.5H3.854c-0.229,0-0.417,0.188-0.417,0.417v14.167c0,0.229,0.188,0.417,0.417,0.417h12.917c0.229,0,0.416-0.188,0.416-0.417V4.952C17.188,4.84,17.144,4.733,17.064,4.656M6.354,3.333h7.917V10H6.354V3.333z M16.354,16.667H4.271V3.333h1.25v7.083c0,0.229,0.188,0.417,0.417,0.417h8.75c0.229,0,0.416-0.188,0.416-0.417V3.886l1.25,1.239V16.667z M13.402,4.688v3.958c0,0.229-0.186,0.417-0.417,0.417c-0.229,0-0.417-0.188-0.417-0.417V4.688c0-0.229,0.188-0.417,0.417-0.417C13.217,4.271,13.402,4.458,13.402,4.688"></path>
+            </svg>
+          </button>
           
           <button type="button" v-if="fullScreen" @click="toggleFullScreenMode" class="cool-lightbox-toolbar__btn" title="Fullscreen">
             <svg width="20px" height="20px" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
@@ -758,6 +764,20 @@ export default {
   },
 
   methods: {
+    Download() {
+      const imageSrc = this.getItemSrc(this.imgIndex)
+      fetch(imageSrc).then(image => {
+        image.blob().then(imageBlob => {
+          const imageURL = URL.createObjectURL(imageBlog)
+          const link = document.createElement('a')
+          link.href = imageURL
+          link.download = 'image file name here'
+          document.body.appendChild(link)
+          link.click()
+          document.body.removeChild(link)
+        })
+      })
+    },
     getExtFromItem(imgIndex) {
       if(imgIndex === null) {
         return false
