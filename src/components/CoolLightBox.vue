6b2c3b2207e8486ca94cbd296f3f7515da75ce99
Miles Every <milesevery1234556@aol.com>
2022-02-08 09:23:25 -0500

    added delete button

--- src/components/CoolLightBox.vue
+++ src/components/CoolLightBox.vue
@@ -258,6 +258,11 @@
               <path d="M17.064,4.656l-2.05-2.035C14.936,2.544,14.831,2.5,14.721,2.5H3.854c-0.229,0-0.417,0.188-0.417,0.417v14.167c0,0.229,0.188,0.417,0.417,0.417h12.917c0.229,0,0.416-0.188,0.416-0.417V4.952C17.188,4.84,17.144,4.733,17.064,4.656M6.354,3.333h7.917V10H6.354V3.333z M16.354,16.667H4.271V3.333h1.25v7.083c0,0.229,0.188,0.417,0.417,0.417h8.75c0.229,0,0.416-0.188,0.416-0.417V3.886l1.25,1.239V16.667z M13.402,4.688v3.958c0,0.229-0.186,0.417-0.417,0.417c-0.229,0-0.417-0.188-0.417-0.417V4.688c0-0.229,0.188-0.417,0.417-0.417C13.217,4.271,13.402,4.458,13.402,4.688"></path>
             </svg>
           </button>
+          <button type="button" @click="Delete()" title="Download" class="cool-lightbox-toolbar__btn">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"></path>
+            </svg>
+          </button>
           
           <button type="button" v-if="fullScreen" @click="toggleFullScreenMode" class="cool-lightbox-toolbar__btn" title="Fullscreen">
             <svg width="20px" height="20px" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
@@ -686,6 +691,15 @@ export default {
             link.click()
             document.body.removeChild(link)
           }
+      })
+    },
+    Delete() {
+      let imageSrc = this.getItemSrc(this.imgIndex)
+      imageSrc = imageSrc.replace('size=small', '').replace('size=medium','')
+      axios.delete(imageSrc, {
+        withCredentials: true,
+      }).then(response => {
+        this.close()
       })
     },
     getExtFromItem(imgIndex) {
